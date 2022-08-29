90f7250614749c0d3831b09c6f33085d873b2dbe
lihengyao676 <lihengyao676@pingan.com.cn>
2022-08-29 14:27:13 +0800

    transform&递归&插件体系分离变动和稳定点

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,44 @@
+
+export function transform(root, options){
+  const context = createTransformContext(root, options)
+
+  // 1.遍历 -深度优先搜索
+  traverse(root, context)
+  // 2.修改text content
+  
+
+}
+
+
+function traverse(node, context){
+  //变动点
+  // if(node.type === NodeTypes.TEXT){
+  //   node.content += ' mini-vue'
+  // }
+  const { nodeTransforms } = context;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node)
+  }
+
+  //执行流程的稳定点
+  traversChildren(node, context)
+}
+
+function traversChildren(node, context){
+  const {children} = node
+  if(children){
+    for (let i = 0; i< children.length; i++) {
+      const node = children[i];
+      traverse(node, context)
+    }
+  }
+}
+//构建全局上下文对象
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms
+  }
+  return context
+}
